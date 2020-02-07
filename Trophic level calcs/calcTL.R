6e5aaaae90b8dceeb0043fa886a7f30195432749
nissandjac@gmail.com <nissandjac@gmail.com>
2020-02-07 12:48:54 -0800

    added TL calcs until 2019

--- Trophic level calcs/calcTL.R
+++ Trophic level calcs/calcTL.R
@@ -5,29 +5,31 @@ library(ggplot2)
 source('weighted_mean.R') # Calculates a weighted mean with NA in weights
 # Get the catches of forage fish 
 
-
-df <- read.csv('marinecatches.csv')
+df.old <- read.csv('marinecatches.csv')
 df <- df[df$size == 'Small',]
 
-# 
+df.new <- read.csv('fao_catches_2019.csv')
+df <- df.new[df.new$size == 'Small',]
+# # 
 yrs <- sort(unique(df$Year))
 
 
 spp <- unique(df$Species)
 
-TL <- rep(NA, length(spp))
-
-# for(i in 1:length(spp)){ # Improve this later with the FishLife method 
-#   
-#   ecotmp <- rfishbase::ecology(spp[i], fields = c('FoodTroph','DietTroph'))  
-#   
+# TL <- rep(NA, length(spp))
+# 
+# for(i in 1:length(spp)){ # Improve this later with the FishLife method
+# 
+#   ecotmp <- rfishbase::ecology(spp[i], fields = c('FoodTroph','DietTroph'))
+# 
 #   if(all(is.na(as.numeric(unlist(ecotmp)))) ==0){
-#     TL[i]<- mean(as.numeric(unlist(ecotmp)), na.rm = TRUE)  
+#     TL[i]<- mean(as.numeric(unlist(ecotmp)), na.rm = TRUE)
 #   }
-#   print(i) # This takes a while 
+#   print(i) # This takes a while
+#   rm(ecotmp)
 # }
-
-# Calculate the trophic level over time   
+# 
+# # Calculate the trophic level over time   
 # df.export <- data.frame(spp = spp, TL= TL)
 # write.csv(df.export, file = 'TL_forage.csv', row.names = FALSE)
 TL <- read.csv('TL_forage.csv')  
@@ -49,7 +51,7 @@ df.tot %>%
 df.tot$TL <- NA
 
 for(i in 1:length(spp)){
-  idx <- which(df.tot$Species == TL$spp[i])
+  idx <- which(as.character(df.tot$Species) == as.character(TL$spp[i]))
   df.tot$TL[idx] <- TL$TL[i]
 }
 
@@ -57,5 +59,6 @@ for(i in 1:length(spp)){
 df.tot %>% 
   group_by(Year) %>% 
   summarise(TL = weighted_mean(TL,Catch, na.rm = TRUE)) %>% 
-  ggplot(aes(x = Year, y = TL))+geom_line(color = alpha('Darkred', alpha = 0.8), size = 1.5)+theme_classic()
+  ggplot(aes(x = Year, y = TL))+geom_line(color = alpha('Darkred', alpha = 0.8), size = 1.5)+theme_classic()+
+  coord_cartesian(ylim = c(2.5,3.5))
 
